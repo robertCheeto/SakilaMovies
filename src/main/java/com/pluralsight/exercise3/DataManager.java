4b5212c2d92c522817eac2f230a897757ba58a6f
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-03 11:34:54 -0500

    * working on creating a datamanager and main class for exercise3 for the sakila movies portion of workbook 8c

---
+++ src/main/java/com/pluralsight/exercise3/DataManager.java
@@ -0,0 +1,105 @@
+package com.pluralsight.exercise3;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DataManager {
+    private DataSource dataSource;
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/sakila";
+    private static Connection connection = null;
+    private static final Scanner keyboard = new Scanner(System.in);
+
+    public void SakilaDataManager(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static void loadConnection(String username, String password) {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            System.out.println("Error when loading connection. Exiting application.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public List<Actors> getActorsByName() {
+        List<Actors> actors = new ArrayList<>();
+
+        System.out.print("Enter the last name of an actor/actress: ");
+        String lastName = keyboard.nextLine();
+
+        String query = "SELECT Actor_ID, First_Name, Last_Name FROM Actor WHERE Last_Name = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, lastName);
+            try (ResultSet results = statement.executeQuery()) {
+                System.out.println("\nListing Actors with the Last Name " + lastName);
+                System.out.println("\n+-+-+-+-+-+-+-+-+-+-+-+");
+                while (results.next()) {
+                    int actorID = results.getInt("actor_id");
+                    String fName = results.getString("first_name");
+                    String lName = results.getString("last_name");
+                    System.out.println(fName + " " + lName);
+
+                    Actors actor = new Actors(actorID, fName, lName);
+                    Actors.add(actor);
+                }
+                System.out.println("+-+-+-+-+-+-+-+-+-+-+-+");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return actors;
+    }
+
+    public List<Movies> getMoviesByActor(){
+        List<Movies> movies = new ArrayList<>();
+
+        System.out.println("Enter the first and last name of an actor/actress you want to see.");
+        System.out.println("All of the movies they star in will be listed.");
+        System.out.print("First Name: ");
+        String firstName = keyboard.nextLine();
+        System.out.print("Last Name: ");
+        String lastName = keyboard.nextLine();
+
+
+        String query = "SELECT F.Title, F.Release_Year, F.Length, F.Rating FROM Film AS F " +
+                "JOIN Film_Actor AS FA ON (FA.Film_ID = F.Film_ID)" +
+                "JOIN Actor AS A ON (A.Actor_ID = FA.Actor_ID)" +
+                "WHERE A.First_Name = ? AND A.Last_Name = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            try (ResultSet results = statement.executeQuery()) {
+                System.out.println("\nMovie Title \t Release Year \t Film Length \t Rating");
+                System.out.println("+-+-+-+-+-+-+-+-+-+-+-+");
+                while (results.next()) {
+                    String movieTitle = results.getString("F.Title");
+                    int movieYear = results.getInt("F.Release_Year");
+                    int movieLength = results.getInt("F.Length");
+                    String movieRating = results.getString("F.Rating");
+                    System.out.printf("%s \t %s \t %s \t %s\n", movieTitle, movieYear, movieLength, movieRating);
+
+                    Movies movie = new Movies(title, year, length, rating);
+                    movies.add(movie);
+                }
+                System.out.println("+-+-+-+-+-+-+-+-+-+-+-+");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+}
--- src/main/java/com/pluralsight/exercise3/MainEx3.java
+++ src/main/java/com/pluralsight/exercise3/MainEx3.java
@@ -1,4 +1,15 @@
 package com.pluralsight.exercise3;
 
+import static com.pluralsight.exercise3.DataManager.loadConnection;
+import com.pluralsight.exercise3.DataManager.
+
 public class MainEx3 {
+    public static void main(String[] args) {
+        loadConnection(args[0], args[1]);
+
+        SakilaDataManager dataManager = new SakilaDataManager(dataSource);
+
+        List<Actors> actors = dataManager.getActorsByName();
+
+    }
 }
