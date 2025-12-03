3b8a775b1dfcafa896f18e1213e879bbe892edbc
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-03 11:55:09 -0500

    * continuing to work on program. unsure if changes made will benefit

--- src/main/java/com/pluralsight/exercise3/DataManager.java
+++ src/main/java/com/pluralsight/exercise3/DataManager.java
@@ -1,6 +1,5 @@
 package com.pluralsight.exercise3;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
@@ -9,27 +8,11 @@ import java.util.Scanner;
 
 public class DataManager {
     private DataSource dataSource;
-    private static final String url = "jdbc:mysql://127.0.0.1:3306/sakila";
     private static Connection connection = null;
     private static final Scanner keyboard = new Scanner(System.in);
 
-    public void SakilaDataManager(DataSource dataSource) {
+    public DataManager(DataSource dataSource) {
         this.dataSource = dataSource;
-    }
-
-    public static void loadConnection(String username, String password) {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-
-        try {
-            connection = dataSource.getConnection();
-        } catch (SQLException e) {
-            System.out.println("Error when loading connection. Exiting application.");
-            e.printStackTrace();
-            System.exit(1);
-        }
     }
 
     public List<Actors> getActorsByName() {
--- src/main/java/com/pluralsight/exercise3/MainEx3.java
+++ src/main/java/com/pluralsight/exercise3/MainEx3.java
@@ -1,13 +1,27 @@
 package com.pluralsight.exercise3;
 
-import static com.pluralsight.exercise3.DataManager.loadConnection;
-import com.pluralsight.exercise3.DataManager.
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.*;
 
 public class MainEx3 {
-    public static void main(String[] args) {
-        loadConnection(args[0], args[1]);
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/sakila";
 
-        SakilaDataManager dataManager = new SakilaDataManager(dataSource);
+    public static void main(String[] args) {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(args[0]);
+        dataSource.setPassword(args[1]);
+
+        try {
+            Connection connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            System.out.println("Error when loading connection. Exiting application.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        DataManager dataManager = new DataManager(dataSource);
 
         List<Actors> actors = dataManager.getActorsByName();
 
