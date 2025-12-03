2821b326655685f283f95f68fa697f7fa26acf9e
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-03 14:47:12 -0500

    * updated Main to only handle "main" functions to run program

--- src/main/java/com/pluralsight/exercise3/MainEx3.java
+++ src/main/java/com/pluralsight/exercise3/MainEx3.java
@@ -1,10 +1,7 @@
 package com.pluralsight.exercise3;
 
-import java.util.Scanner;
-
 public class MainEx3 {
-    private static final Scanner keyboard = new Scanner(System.in);
-    private static boolean isRunning = true;
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Sakila Movie Database");
 
@@ -18,20 +15,6 @@ public class MainEx3 {
 
         DataManager dataManager = new DataManager(username, password);
 
-        while(isRunning) {
-            UI.displayMainMenu();
-            int userInput = keyboard.nextInt();
-            keyboard.nextLine();
-
-            switch (userInput) {
-                case 1 -> displayActors();
-                case 2 -> displayMovies();
-                case 99 -> isRunning = false;
-                default -> System.out.println("Enter a valid input");
-            }
-            System.out.println("Program shutting down...");
-            keyboard.close();
-            System.exit(0);
-        }
+        AppController.runProgram(dataManager);
     }
 }
